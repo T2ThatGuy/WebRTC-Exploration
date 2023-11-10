9db89d3b1366f30a1c0aa6598b02a7c9b2ad5a2f
Charlie <66382380+T2ThatGuy@users.noreply.github.com>
2023-11-10 16:28:49 +0000

    :sparkles: Initial AculabCloud provider created

---
+++ src/providers/AculabCloud.tsx
@@ -0,0 +1,98 @@
+import {
+    AculabBaseClass,
+    getToken,
+} from '@aculab-com/react-native-aculab-client';
+import {
+    ReactNode,
+    createContext,
+    useCallback,
+    useContext,
+    useMemo,
+    useState,
+} from 'react';
+
+const AculabCloudContext = createContext(
+    {} as {
+        client: null;
+        webRTCToken: string;
+        register: (options: RegisterOptions) => Promise<void>;
+    },
+);
+
+type RegisterOptions = {
+    cloudRegionId: string;
+    webRTCAccessKey: string;
+    registerClientId: string;
+    logLevel: string | number;
+    cloudUsername: string;
+    apiAccessKey: string;
+};
+
+function AculabCloudProvider({ children }: { children: ReactNode }) {
+    const [client, setClient] = useState(null);
+    const [webRTCToken, setWebRTCToken] = useState('');
+
+    const register = useCallback(
+        async ({
+            cloudRegionId,
+            webRTCAccessKey,
+            registerClientId,
+            logLevel,
+            cloudUsername,
+            apiAccessKey,
+        }: RegisterOptions) => {
+            const newWebRTCToken = await getToken({
+                registerClientId: registerClientId,
+                tokenLifeTime: 3600,
+                enableIncomingCall: true,
+                callClientRange: '*',
+                cloudRegionId: cloudRegionId,
+                cloudUsername: cloudUsername,
+                apiAccessKey: apiAccessKey,
+            });
+
+            if (!newWebRTCToken) {
+                return;
+            }
+
+            setWebRTCToken(newWebRTCToken);
+
+            const newClient = await AculabBaseClass.register(
+                cloudRegionId,
+                webRTCAccessKey,
+                registerClientId,
+                logLevel,
+                newWebRTCToken,
+            );
+
+            if (newClient) {
+                AculabBaseClass._client = newClient;
+                setClient(newClient);
+            }
+        },
+        [],
+    );
+
+    const values = useMemo(
+        () => ({ client, webRTCToken, register }),
+        [client, webRTCToken, register],
+    );
+
+    return (
+        <AculabCloudContext.Provider value={values}>
+            {children}
+        </AculabCloudContext.Provider>
+    );
+}
+
+export default AculabCloudProvider;
+export const useAculabCloud = () => {
+    const context = useContext(AculabCloudContext);
+    if (!context) {
+        throw new Error(
+            'useAculabCloud should be used within the context of a AculabCloudProvider component',
+        );
+    }
+
+    return context;
+};
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,8 +1,14 @@
 import { ReactNode } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AculabCloudProvider from '@providers/AculabCloud';
+
 function Providers({ children }: { children: ReactNode }) {
-    return <NavigationContainer>{children}</NavigationContainer>;
+    return (
+        <NavigationContainer>
+            <AculabCloudProvider>{children}</AculabCloudProvider>
+        </NavigationContainer>
+    );
 }
 
 export default Providers;
