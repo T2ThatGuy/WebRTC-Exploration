67cc7bd61434680025ccbf6fbaa3f599e2d0776e
Charlie <66382380+T2ThatGuy@users.noreply.github.com>
2023-11-10 14:55:06 +0000

    :art: Router changes
    - Register loads first on open
    - Simplified header styling

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,27 +1,32 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+    createNativeStackNavigator,
+    NativeStackNavigationOptions,
+} from '@react-navigation/native-stack';
 
 import { COLOURS } from '@/styles';
 import { HomeScreen, RegisterScreen } from '@/screens';
 
 const Stack = createNativeStackNavigator();
+const HeaderStyling: NativeStackNavigationOptions = {
+    headerTintColor: 'white',
+    headerTitleAlign: 'center',
+    headerStyle: {
+        backgroundColor: COLOURS.BACKGROUND,
+    },
+};
 
 function Router() {
     return (
         <Stack.Navigator>
             <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{
-                    title: 'Home Screen',
-                    headerTintColor: 'white',
-                    headerTitleAlign: 'center',
-                    headerStyle: { backgroundColor: COLOURS.BACKGROUND },
-                }}
-            />
-            <Stack.Screen
                 name="Register"
                 component={RegisterScreen}
-                options={{ title: 'Register Screen' }}
+                options={{ title: 'Register Screen', ...HeaderStyling }}
+            />
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{ title: 'Home Screen', ...HeaderStyling }}
             />
         </Stack.Navigator>
     );
