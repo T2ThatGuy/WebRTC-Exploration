9fdd21481687d1829521dbd8bf3869fc092dd6a8
Charlie <66382380+T2ThatGuy@users.noreply.github.com>
2023-11-13 15:37:01 +0000

    :heavy_plus_sign: RN Vector Icons + WebRTC
    Do note React Native Vector Icons needs setting up on IOS via xcode, view docs here to do so
    
    https://www.npmjs.com/package/react-native-vector-icons#ios-setup

--- android/app/src/main/java/com/webrtcexploration/MainApplication.java
+++ android/app/src/main/java/com/webrtcexploration/MainApplication.java
@@ -10,6 +10,9 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
+// For React Native Vector Icons
+import com.oblador.vectoricons.VectorIconsPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -25,6 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+
+          // React Native Vector Icons Added
+          // packages.add(new VectorIconsPackage());
+
           return packages;
         }
 
