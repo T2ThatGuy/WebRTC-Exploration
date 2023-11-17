c9823faf1b147d7732f0eb71b3a3afb14ded046c
CharlieS <charlie.stone@aculab.com>
2023-11-17 09:50:13 +0000

    :lipstick: Better SelectDropdown styling

--- src/components/ui/SelectDropdown.tsx
+++ src/components/ui/SelectDropdown.tsx
@@ -2,6 +2,8 @@ import { COLOURS } from '@/styles';
 import { StyleSheet } from 'react-native';
 import RNSelectDropdown from 'react-native-select-dropdown';
 
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
 type SelectDropdownProps<TData extends unknown> = {
     data: TData[];
     onSelect: (value: TData) => void;
@@ -25,6 +27,22 @@ function SelectDropdown<T extends unknown>({
             buttonStyle={style.button}
             buttonTextStyle={style.buttonText}
             // Dropdown
+            dropdownStyle={style.dropdown}
+            rowStyle={style.rowStyle}
+            rowTextStyle={style.rowTxtStyle}
+            renderDropdownIcon={(isOpened) => {
+                return (
+                    <MaterialIcon
+                        name={
+                            isOpened
+                                ? 'keyboard-arrow-up'
+                                : 'keyboard-arrow-down'
+                        }
+                        color={COLOURS.PRIMARY}
+                        size={18}
+                    />
+                );
+            }}
         />
     );
 }
@@ -33,16 +51,32 @@ export { SelectDropdown };
 
 export const style = StyleSheet.create({
     button: {
-        padding: 10,
-        elevation: 8,
         width: 'auto',
         borderRadius: 10,
-        borderColor: 'white',
         borderWidth: 1,
+        borderColor: COLOURS.INPUT_BORDER,
         backgroundColor: undefined,
     },
     buttonText: {
         color: COLOURS.PRIMARY,
         textAlign: 'left',
+        fontSize: 14,
+        marginHorizontal: 0,
+    },
+    dropdown: {
+        borderBottomLeftRadius: 12,
+        borderBottomRightRadius: 12,
+        borderColor: COLOURS.PRIMARY,
+        borderWidth: 1,
+        backgroundColor: COLOURS.BACKGROUND,
+    },
+    rowStyle: {
+        backgroundColor: COLOURS.BACKGROUND,
+        borderBottomColor: COLOURS.PRIMARY,
+    },
+    rowTxtStyle: {
+        color: COLOURS.PRIMARY,
+        textAlign: 'center',
+        fontWeight: 'bold',
     },
 } as const);
